07fe433e201c06c84bb45433b3be2147535b7526
pheufr <pheufr@github.com>
2026-07-27 09:30:52 +0100

    Fix loading of multiple integrations

--- custom_components/raven_castle_jobs/config_flow.py
+++ custom_components/raven_castle_jobs/config_flow.py
@@ -30,10 +30,7 @@ class RavenCastleJobsConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         if self._async_current_entries():
             return self.async_abort(reason="single_instance_allowed")
 
-        if user_input is not None:
-            return self.async_create_entry(title="Raven Castle Jobs", data={})
-
-        return self.async_show_form(step_id="user", data_schema=vol.Schema({}))
+        return self.async_create_entry(title="Raven Castle Jobs", data={})
 
     @staticmethod
     @callback
--- custom_components/raven_castle_quiz/config_flow.py
+++ custom_components/raven_castle_quiz/config_flow.py
@@ -29,10 +29,7 @@ class RavenCastleQuizConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         if self._async_current_entries():
             return self.async_abort(reason="single_instance_allowed")
 
-        if user_input is not None:
-            return self.async_create_entry(title="Raven Castle Quiz", data={})
-
-        return self.async_show_form(step_id="user", data_schema=vol.Schema({}))
+        return self.async_create_entry(title="Raven Castle Quiz", data={})
 
     @staticmethod
     @callback
